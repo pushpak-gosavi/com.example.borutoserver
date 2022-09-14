f3437ce22ed469d0fe433efcc8b7186edbf7791b
Pushpak Gosavi (NCS) <pushpak.gosavi@in.ncs-i.com>
2022-09-14 17:30:38 +0530

    added Hero model

---
+++ src/main/kotlin/com/example/models/ApiResponse.kt
@@ -0,0 +1,9 @@
+package com.example.models
+
+data class ApiResponse(
+    val success:Boolean,
+    val message:String? = null,
+    val prevPage:Int? =null,
+    val nextPage:Int? = null,
+    val heroes:List<Hero> = emptyList()
+)
---
+++ src/main/kotlin/com/example/models/Hero.kt
@@ -0,0 +1,15 @@
+package com.example.models
+
+data class Hero(
+    val id:Int,
+    val name: String,
+    val image:String,
+    val about:String,
+    val rating:Double,
+    val power:Int,
+    val month:String,
+    val day:String,
+    val family:List<String>,
+    val ability:List<String>,
+    val natureTypes:List<String>
+)
